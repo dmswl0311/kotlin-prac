aa36fbe824d0ab24f7745cd0a1a24ca62d5f7de7
dmswl0311 <eunji.cho2@cj.net>
2022-09-23 16:17:28 +0900

    init: 프로젝트 생성

---
+++ backend/prac/src/test/kotlin/com/kotlin/prac/PracApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.prac
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
